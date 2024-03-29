26f70b03782c6acc0e9ec7780b61aada3f46da77
Yash-Gupta001 <gyash4963@gmail.com>
2024-03-29 17:17:30 +0530

    .

--- basics/lib/2_bottom_nav_screens/user.dart
+++ basics/lib/2_bottom_nav_screens/user.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../authentication/phone.dart';
+
 //import 'package:basics/authentication/phone.dart';
 
 class UserScreen extends StatelessWidget {
@@ -6,23 +8,52 @@ class UserScreen extends StatelessWidget {
 
   late String? name,phone;
 
-
 UserScreen({ this.name, this.phone});
 
   Widget build(BuildContext context) {
     
 return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 80,
-        title: const Text("User Screen"),
+        toolbarHeight: 50,
+        title: const Text(
+          "USER",
+          style: TextStyle(
+            color: Colors.white,
+            backgroundColor: Colors.amber,
+            fontSize: 28,
+          ),
+        ),
       ),
-      body:const Column(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text('Name: YASH'),
-          Text('Phone no.: 8805570037'),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Name: YASH',
+              style: TextStyle(fontSize: 18.0),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Phone no.: 8805570037',
+              style: TextStyle(fontSize: 18.0),
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyPhone()),
+              );
+            },
+            child: const Text('LogOut'),
+          ),
         ],
       ),
-      
     );
   }
 }
